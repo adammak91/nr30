305d9994cfe052210b18c16ad61d3ad2b9f17478
adammak91 <adam210125@o2.pl>
2022-02-04 12:31:08 +0100

    zadanie30

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let dowolna = document.querySelector('p');
+
+console.log(dowolna);
+
+
+
+p = element.textContent
+
+element.textContent = ("przykładowy tekst");
+document.getElement("p").textContent = "przykładowy tekst";
+console.log('przykladowy tekst');
